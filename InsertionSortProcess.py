7d37aac8d2ce4be754f104d71213935b8c1f2bf9
Owen <karpfy16@gmail.com>
2020-04-01 22:25:43 -0400

    Realized that previous implementation was to naive, created InsertionSorter class for Insertionsort processor

---
+++ InsertionSortProcess.py
@@ -0,0 +1,23 @@
+from multiprocessing import Process
+from Algorithms import Sorts
+
+
+class InsertionSorter(Process):
+    num_sorted = 0
+    num_array = []
+
+    def __init__(self, params):
+        super(InsertionSorter, self).__init__()
+        self.num_array = params
+
+    def run(self):
+        self.insertion_sort(self.num_array)
+
+    def insertion_sort(self, params):
+        for index in range(1, len(params)):
+            num = index
+            while num >= 1 and params[num] < params[num - 1]:
+                Sorts.swap(params, num - 1, num)
+                num -= 1
+            self.num_sorted += 1
+        return params
--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import random
 from multiprocessing import Process
+from InsertionSortProcess import InsertionSorter
 
 from Algorithms import Sorts
 
@@ -10,21 +11,15 @@ class SortingProcesses:
         pass
 
     def processes(self):
-        num_array = SortingProcesses.array_generator()
-        insertion_sorter = Process(target=Sorts.insertion_sort, args=(num_array,))
-        selection_sorter = Process(target=Sorts.selection_sort, args=(num_array,))
-        merge_sorter = Process(target=Sorts.merge_sort, args=(num_array,))
-        quick_sorter = Process(target=Sorts.quick_sort, args=(num_array,))
+        num_array = SortingProcesses.array_generator(50)
+        insertion_sorter = InsertionSorter(num_array)
         insertion_sorter.start()
-        selection_sorter.start()
-        merge_sorter.start()
-        quick_sorter.start()
 
 
     @staticmethod
     def array_generator(size):
         num_array = []
-        for index in range(0, size):
+        for index in range(0, size+1):
             num_array.append(random.randint(0, 2147483647-1))
         return num_array
 
