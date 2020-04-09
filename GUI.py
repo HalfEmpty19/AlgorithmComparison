400f3ee92d2ae70e644147b26b4726ebedd0b63a
Owen <karpfy16@gmail.com>
2020-04-09 11:31:07 -0400

    Began creation of GUI

---
+++ GUI.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+
+window = tk.Tk()
+
+window.title("Algorithm Speed Comparison")
+
+window.geometry("800x800")
+
+
+
+window.mainloop()
